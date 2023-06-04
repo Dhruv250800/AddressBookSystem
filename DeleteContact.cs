0a8d995b80d5743390c10af383f763a07113942d
DhruvGour <dhruv250800@gmail.com>
2023-06-05 01:31:27 +0530

    [DhruvGour]-adding DeleteContactManager

---
+++ DeleteContact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    using System;
+
+    public class DeleteContactManager
+    {
+        public void DeleteContact(AddressBook addressBook)
+        {
+            Console.Write("Enter the first name of the contact to delete: ");
+            string firstName = Console.ReadLine();
+
+            bool contactDeleted = addressBook.DeleteContactByFirstName(firstName);
+            if (contactDeleted)
+            {
+                Console.WriteLine("Contact deleted successfully!");
+            }
+            else
+            {
+                Console.WriteLine("Contact not found!");
+            }
+        }
+    }
+}
