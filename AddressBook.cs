af65b567453b6beabd1a7bbe4dc4f81501b206b6
DhruvGour <dhruv250800@gmail.com>
2023-06-05 01:20:54 +0530

    [DhruvGour]-adding addressBook

---
+++ AddressBook.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class AddressBook
+    {
+        private List<Contact> contacts = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            contacts.Add(contact);
+        }
+
+        public void ListContacts()
+        {
+            foreach (var contact in contacts)
+            {
+                Console.WriteLine($"First Name: {contact.FirstName}");
+                Console.WriteLine($"Last Name: {contact.LastName}");
+                Console.WriteLine($"Address: {contact.Address}");
+                Console.WriteLine($"City: {contact.City}");
+                Console.WriteLine($"State: {contact.State}");
+                Console.WriteLine($"Zip: {contact.Zip}");
+                Console.WriteLine($"Phone Number: {contact.PhoneNumber}");
+                Console.WriteLine($"Email: {contact.Email}");
+                Console.WriteLine();
+            }
+            Contact GetContactByFirstName(string firstName)
+            {
+                return contacts.FirstOrDefault(c => c.FirstName.Equals(firstName, StringComparison.OrdinalIgnoreCase));
+            }
+
+            bool DeleteContactByFirstName(string firstName)
+            {
+                Contact contactToDelete = GetContactByFirstName(firstName);
+                if (contactToDelete != null)
+                {
+                    contacts.Remove(contactToDelete);
+                    return true;
+                }
+                return false;
+            }
+
+
+
+        }
+
+        internal bool DeleteContactByFirstName(string? firstName)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal Contact GetContactByFirstName(string? firstName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("welcome to AddressBookSystem ");
+        AddressBook addressBook = new AddressBook();
+        EditContactManager editContactManager = new EditContactManager();
+        DeleteContactManager deleteContactManager = new DeleteContactManager();
+
+        Console.WriteLine("Address Book Application");
+        Console.WriteLine("------------------------");
+
+        while (true)
+        {
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. Edit Contact");
+            Console.WriteLine("3. Delete Contact");
+            Console.WriteLine("4. List Contacts");
+            Console.WriteLine("5. Exit");
+
+            Console.Write("Enter your choice: ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int choice))
+            {
+                Console.WriteLine("Invalid choice! Please enter a valid integer.");
+                Console.WriteLine();
+                continue;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the contact details:");
+                    Contact newContact = new Contact();
+                    Console.Write("First Name: ");
+                    newContact.FirstName = Console.ReadLine();
+                    Console.Write("Last Name: ");
+                    newContact.LastName = Console.ReadLine();
+                    Console.Write("Address: ");
+                    newContact.Address = Console.ReadLine();
+                    Console.Write("City: ");
+                    newContact.City = Console.ReadLine();
+                    Console.Write("State: ");
+                    newContact.State = Console.ReadLine();
+                    Console.Write("Zip: ");
+                    newContact.Zip = Console.ReadLine();
+                    Console.Write("Phone Number: ");
+                    newContact.PhoneNumber = Console.ReadLine();
+                    Console.Write("Email: ");
+                    newContact.Email = Console.ReadLine();
+
+                    addressBook.AddContact(newContact);
+                    Console.WriteLine("Contact added successfully!");
+                    break;
+
+                case 2:
+                    editContactManager.EditContact(addressBook);
+                    break;
+
+                case 3:
+                    deleteContactManager.DeleteContact(addressBook);
+                    break;
+
+                case 4:
+                    Console.WriteLine("List of Contacts:");
+                    addressBook.ListContacts();
+                    break;
+
+                case 5:
+                    Console.WriteLine("Exiting the program...");
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid choice! Please try again.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
